ddb146119021b24aef1894d2ecc9c23fc3942e3f
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2024-07-13 18:24:22 -0500

    Style site and page headers

--- config.mjs
+++ config.mjs
@@ -2,4 +2,8 @@ export const site = {
   title: "Our Family Website",
   description: "A website for our family.",
   url: "https://ourfamilywebsite.netlify.app",
+  author: {
+    name: "Sam Feldstein",
+    email: "sam@samfeldstein.xyz",
+  }
 };
