5894ad351b3cfa227a455607f7220cf2e3f048c9
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2024-07-13 16:11:12 -0500

    Add service worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://ourfamilywebsite.netlify.app",
+  prefetch: {
+    prefetchAll: true,
+  },
+  vite: {
+    css: {
+      transformer: "sass",
+    },
+  },
+});
