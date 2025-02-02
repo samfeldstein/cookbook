9e8e3690d3dd30cc9c1249a5348f53cde477859c
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2025-02-02 16:21:50 -0700

    Optimize
    Remove some packages, add some packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ourfamilywebsite.netlify.app",
+  site: "https://our-family-cookbook.netlify.app",
   prefetch: {
     prefetchAll: true,
   }
