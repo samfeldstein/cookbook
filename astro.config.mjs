4109c0f1a25d97b12fd441e02872ae758ad6c508
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2025-06-10 13:25:12 -0600

    Udate site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cookbook.samfeldstein.xyz/",
+  site: "https://cookbook.samfeldstein.xyz",
   prefetch: {
     prefetchAll: true,
   }
