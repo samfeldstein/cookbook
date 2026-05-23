4d6847c49a9df9c351a16b1f691e4003657e2d15
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2026-05-23 15:41:31 -0600

    Fix weight of strong tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://cookbook.samfeldstein.xyz",
+  server: { host: true },
   prefetch: {
     prefetchAll: true,
   }
