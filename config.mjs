c6b976c6e4c7d36aa808bc6d808d3dba0b8f817d
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2026-05-24 09:27:10 -0600

    Update email

--- config.mjs
+++ config.mjs
@@ -4,6 +4,6 @@ export const site = {
   url: "https://cookbook.samfeldstein.xyz/",
   author: {
     name: "Sam Feldstein",
-    email: "sam@samfeldstein.xyz",
+    email: "samfeldstein@proton.me",
   }
 };
