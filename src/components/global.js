555d2e2288c51465dd8f153d6b3f294859232d5b
Sam Feldstein <106436328+seldstein@users.noreply.github.com>
2026-05-28 11:19:32 -0600

    Abstract recipe components

--- src/components/global.js
+++ src/components/global.js
@@ -1,3 +1,5 @@
 import Recipe from "./recipes/Recipe.astro";
+import Notes from "./recipes/Notes.astro";
+import Footnotes from "./recipes/Footnotes.astro";
 
-export const components = { Recipe }
+export const components = { Recipe, Notes, Footnotes }
